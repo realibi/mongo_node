3ef321e14b552efe2eabd88028e441b3fd972e8e
realibi <reallibi@gmail.com>
2022-06-01 15:24:02 +0600

    mongodb connected, cars router added

---
+++ Models.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const schemas = require("./Schemas.js");
+
+const Car = mongoose.model("Car", schemas.CarSchema);
+const User = mongoose.model("User", schemas.UserSchema);
+
+module.exports = {
+    Car,
+    User
+}
---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+const carsRouter = require("./routers/carsRouter");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use("/cars", carsRouter);
+
+const CONNECTION_STRING = "mongodb+srv://realibi:intersekt01@cluster0.8rc2y.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(CONNECTION_STRING, function(err){
+        if(err) return console.log(err);
+        app.listen(8080);
+    }
+);
